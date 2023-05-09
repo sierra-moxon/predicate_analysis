1fd1fcadc45c99dcbf0209c116be6c13f4cf9090
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-05-09 12:44:08 -0700

    add treats as an optional param

--- predicate_analysis/predicate_usage.py
+++ predicate_analysis/predicate_usage.py
@@ -8,7 +8,7 @@ tk = Toolkit('https://raw.githubusercontent.com/biolink/biolink-model/v3.2.8/bio
 tsv_report = open("report.tsv", "a")
 tsv_writer_att = csv.writer(tsv_report, delimiter='\t')
 predicates_to_test = ["biolink:ameliorates", "biolink:prevents", "biolink:affects_risk_for",
-                      "biolink:predisposes", "biolink:contraindicated_for", "biolink:exacerbates"]
+                      "biolink:predisposes", "biolink:contraindicated_for", "biolink:exacerbates", "biolink:treats"]
 
 
 def main():
@@ -18,39 +18,35 @@ def main():
     except ConnectionError:
         print("unable to fetch from smartapi")
         return {}
-    collection_map = {}
-    collected_maps = []
-    for hit in smartapi_docs.get('hits'):
-        # print(hit.get('info').get('x-translator').get('team'))
-        url = hit.get('servers')[0].get('url')
-        if url.endswith('/'):
-            url = url[:-1]
-        metakg_url = f'{url}/meta_knowledge_graph'
-        collection_edges = []
-        if metakg_url == 'https://cam-kp-api.transltr.io/1.3.0/meta_knowledge_graph' or \
-           metakg_url == 'https://cam-kp-api.ci.transltr.io/1.3.0/meta_knowledge_graph' or \
-           metakg_url == 'https://cam-kp-api-dev.transltr.io/1.3.0/meta_knowledge_graph':
-            continue
-        else:
-            try:
-                response = requests.get(metakg_url)
-                if response.status_code == 200:
-                    for id in predicates_to_test:
-                        for edge in response.json().get("edges"):
-                            if edge.get("predicate") == id:
-                                # map of edges per predicate for a source
-                                print(hit.get('info').get('title') + ": " + metakg_url)
-                                # print(hit.get('info').get('title'))
-                                pprint(edge)
-                                printable_edge = (hit.get('info').get('title') + ": " + metakg_url,
-                                                  edge.subject,
-                                                  edge.predicate,
-                                                  edge.object)
-                                collection_edges.append(printable_edge)
-            except Exception as e:
+    with open("predicate_edges.tsv", 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['source', 'subject', 'predicate', 'object'])
+        for hit in smartapi_docs.get('hits'):
+            # print(hit.get('info').get('x-translator').get('team'))
+            url = hit.get('servers')[0].get('url')
+            if url.endswith('/'):
+                url = url[:-1]
+            metakg_url = f'{url}/meta_knowledge_graph'
+            if metakg_url == 'https://cam-kp-api.transltr.io/1.3.0/meta_knowledge_graph' or \
+                metakg_url == 'https://cam-kp-api.ci.transltr.io/1.3.0/meta_knowledge_graph' or \
+                metakg_url == 'https://cam-kp-api-dev.transltr.io/1.3.0/meta_knowledge_graph':
                 continue
-    with open("edges_using_predicates.json", 'w') as f:
-        json.dump(collection_edges, f, indent=4)
+            else:
+                try:
+                    response = requests.get(metakg_url)
+                    if response.status_code == 200:
+                        for id in predicates_to_test:
+                            for edge in response.json().get("edges"):
+                                if edge.get("predicate") == id:
+                                    # map of edges per predicate for a source
+                                    source = hit.get('info').get('title') + ": " + metakg_url
+                                    # print(hit.get('info').get('title'))
+                                    printable_edge = (source, edge.get("subject"), edge.get("predicate"), edge.get("object"))
+                                    print(printable_edge)
+                                    writer.writerow(printable_edge)
+
+                except Exception as e:
+                    continue
     return smartapi_docs
 
 
